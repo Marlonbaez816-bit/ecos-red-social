41dd9e8fe53774717d7255f92ab6b6ca4aef36d0
MBMsocialprogram <marlonbaez816@gmail.com>
2026-06-25 16:06:38 -0400

    Agregado: js/config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export const ECOS_CONFIG = {
+  auth: { url: 'https://ecos-auth.supabase.co', key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjb3MtYXV0aCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzE5MjM0NTY3LCJleHAiOjIwMzQ4MDU0Njd9.ejemploClaveAuth1234567890' },
+  content: { url: 'https://ecos-content.supabase.co', key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjb3MtY29udGVudCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzE5MjM0NTY4LCJleHAiOjIwMzQ4MDU0Njh9.ejemploClaveContent1234567890' },
+  social: { url: 'https://ecos-social.supabase.co', key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjb3Mtc29jaWFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyMzQ1NjksImV4cCI6MjAzNDgwNTQ2OX0.ejemploClaveSocial1234567890' }
+};
